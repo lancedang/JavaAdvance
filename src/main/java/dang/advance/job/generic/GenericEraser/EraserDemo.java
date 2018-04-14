3b204593bd3d0b8b4912e84f8558a9abaf11d874
dangdang <youlangukong@163.com>
2018-04-14 10:58:42 +0800

    加入泛型类案例

--- src/main/java/dang/advance/job/generic/GenericEraser/EraserDemo.java
+++ src/main/java/dang/advance/job/generic/GenericEraser/EraserDemo.java
@@ -18,6 +18,7 @@ public class EraserDemo {
         Class<? extends List> aClass = list.getClass();
         //找到名字为 add 且参数为 Integer类型的方法，运行报错
         //因为List源码中 add 方法的参数 是 E 类型参数，不存在以 Integer 为参数的方法
+        //java.lang.NoSuchMethodException: java.util.ArrayList.add(java.lang.Integer)
         //从中也可看出，唯一标识某个方法的除了方法名外还要加上 方法参数
         //Method addMethod = aClass.getDeclaredMethod("add", Integer.class);
 
