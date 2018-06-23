9191209a84e82a31078ec250f618ba3f936fbc35
dangdang <youlangukong@163.com>
2018-06-23 22:03:32 +0800

    添加 modifier demo

---
+++ src/main/java/dang/advance/job/modifier/Test1.java
@@ -0,0 +1,14 @@
+package dang.advance.job.modifier;
+
+/**
+ * Created by Dangdang on 2018/6/23.
+ */
+public class Test1 {
+
+    //同 package 或者不同 package 但是是 Test1 的子类
+    protected int x = 5;
+
+    //friendly 权限，访问必须同 package
+    int y = 10;
+
+}
---
+++ src/main/java/dang/advance/job/modifier/test/Test2.java
@@ -0,0 +1,30 @@
+package dang.advance.job.modifier.test;
+
+import dang.advance.job.modifier.Test1;
+
+/**
+ * Created by Dangdang on 2018/6/23.
+ */
+public class Test2 extends Test1 {
+
+    public static void main(String[] args) {
+
+        //Test1 的 x 是 protected
+
+        Test1 test1 = new Test1();
+        //Test2 和 Test1 不同 package，且实例 test1 不是继承的 Test1
+        //System.out.println(test1.x);
+
+        Test2 test2 = new Test2();
+        //虽 Test2 与 Test1 不同包, 但 Test2 继承 Test1
+        System.out.println(test2.x);
+
+        System.out.println("......................................");
+
+        //y 在 Test1 中是 friendly 权限，只能在同 package 下访问
+        //System.out.println(test1.y);
+        //就算继承也不可以访问必须同 package
+        //System.out.println(test2.y);
+    }
+
+}
