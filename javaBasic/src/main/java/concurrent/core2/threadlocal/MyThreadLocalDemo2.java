aaf54394720370300a7c2e76f3c00fc719788795
dangdang <dangdang@mail.com>
2024-12-20 15:00:11 +0800

    添加threadLocal demo

--- javaBasic/src/main/java/concurrent/core2/threadlocal/MyThreadLocalDemo2.java
+++ javaBasic/src/main/java/concurrent/core2/threadlocal/MyThreadLocalDemo2.java
@@ -1,21 +1,19 @@
 package concurrent.core2.threadlocal;
 
-import concurrent.core2.weakreference.upper.A;
+import concurrent.core2.ClockUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Slf4j
 public class MyThreadLocalDemo2 {
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+    public static void main2(String[] args) throws NoSuchFieldException, IllegalAccessException, InterruptedException {
 
         Thread thread1 = new Thread(() -> {
             try {
-                test("aaa", false);
+                checkKeyIsNull("aaa", false);
             } catch (NoSuchFieldException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
@@ -30,30 +28,42 @@ public class MyThreadLocalDemo2 {
         System.out.println("main");
     }
 
-    public static void test(String s, boolean isGc) throws NoSuchFieldException, IllegalAccessException {
+    public static void checkKeyIsNull(String yourData, boolean isGc) throws NoSuchFieldException, IllegalAccessException {
 
         Thread thread = Thread.currentThread();
 
-        ThreadLocal<Object> local = new ThreadLocal<>();
-
-        Object o = local.get();
-
-        System.out.println(o);
+//        ThreadLocal<Object> local = new ThreadLocal<>();
+//
+//        Object o = local.get();
+//
+//        System.out.println(o);
 
         ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-
-
-        threadLocal.set(s);
+        threadLocal.set(yourData);
 
         if (isGc) {
             System.gc();
         }
 
+        showEntryInThread(thread);
 
+
+    }
+
+    //目的：gc时，判断某个线程的threadLocal实例(也就是key) 是否为null
+    //背景：Thread内部的threadLocals 成员变量，没有通过api暴露出来，
+    //也就无法直接查看其内部的entry数组的key
+    //(key即threadLocal实例，也是Entry父类WeakReference的referent属性）
+    //故，下面通过，对Thread进行反射，一步步获取其entry数组，进而找到数组每个元素的key
+
+    //Thread->ThreadLocal.ThreadLocalMap->Entry[]->(value比较直接)
+    //Thread->ThreadLocal.ThreadLocalMap->Entry[]->父WeakReference->referent(threadLocal实例)
+    private static void showEntryInThread(Thread thread) throws NoSuchFieldException, IllegalAccessException {
         Class<? extends Thread> aClass = thread.getClass();
 
-        //获取属性
+        //step1: 先获取Thread类的成员变量-threadLocals（这个是ThreadLocalMap类型的）
+        //Thread 源代码：ThreadLocal.ThreadLocalMap threadLocals = null;
         Field threadLocalsField = aClass.getDeclaredField("threadLocals");
 
         threadLocalsField.setAccessible(true);
@@ -62,22 +72,29 @@ public class MyThreadLocalDemo2 {
         Object threadLocalMapObject = threadLocalsField.get(thread);
 
         Class<?> threadLocalMapClass = threadLocalMapObject.getClass();
+        //step2： 再获取ThreadLocalMap的成员变量-table
         Field tableField = threadLocalMapClass.getDeclaredField("table");
 
         tableField.setAccessible(true);
         Object[] tableEntryObjectArray = (Object[]) tableField.get(threadLocalMapObject);
 
+        //step3: 遍历这个table数组
         for (Object entry : tableEntryObjectArray) {
             //entry有一个value，和继承自父类的referent属性
             if (Objects.isNull(entry)) {
                 continue;
             }
+
+            //step4： entry是继承自WeakReference的子类
             Class<?> entryClass = entry.getClass();
-            //entry自己定义的可以直接获取
+
+            //step4-1: entry自己定义的属性可以直接获取
             Field entryValueField = entryClass.getDeclaredField("value");
             entryValueField.setAccessible(true);
             Object entryValue = entryValueField.get(entry);
 
+            //step4-2: entry 父类WeakReference的属性referent 获取之
+            //最终的目的：是判断这个referent是否为null
             //referent是父类的，需要使用父类的class获取,准确的 说是父类的父类
             //这个属性也就是通常说的key
             //这个很重要：referent指向的是被weakReference封装的真实对象
@@ -85,12 +102,84 @@ public class MyThreadLocalDemo2 {
             referentFiled.setAccessible(true);
             Object entryKey = referentFiled.get(entry);
 
-            //log.info("threadLocalMap key={}, value={}", entryKey, entryValue);
-            System.out.println("threadLocalMap map的 key是啥= " + entryKey + ";key类型是="+ entryKey.getClass() + "； value又是啥=" + entryValue);
+            if (entryKey != null) {
+                if (entryKey.getClass() != ThreadLocal.class) {
+                    continue;
+                }
+
+                //log.info("threadLo   calMap key={}, value={}", entryKey, entryValue);
+                log.info("threadLocalMap map的 key={},key类型是={}, value又是啥={}", entryKey, entryKey.getClass(), entryValue);
+            } else {
+                log.info("threadLocalMap entry 的entryKey是null");
+            }
 
         }
-
-
     }
 
+    @Test
+    //场景1：无强引用指向 ThreadLocal()实例，查看gc前后 该对象 是否被回收
+    public void testNoStrongYinyong() {
+        Runnable noStrongYinYong = new Runnable() {
+            @Override
+            public void run() {
+                Thread curThread = Thread.currentThread();
+
+                //1.先给当前线程设置一个threadLocal, 形式：直接使用new ThreadLocal()这玩意
+                //以验证 new ThreadLocal()这个对象 无强引用情况下，gc 前后，
+                // ThreadLocalMap 的Entry[]数组里，以这个对象(new ThreadLocal())为key的key会不会被回收
+                new ThreadLocal<String>().set("业务数据");
+
+                try {
+                    log.info("gc前，查看 new ThreadLocal()形式的key在Entry数组中是否为null");
+                    showEntryInThread(curThread);
+
+                    log.info("--------------------------");
+
+                    log.info("gc后，查看 new ThreadLocal()形式的key在Entry数组中是否为null");
+                    System.gc();
+                    showEntryInThread(curThread);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new Thread(noStrongYinYong).start();
+
+        ClockUtil.sleep(1000);
+    }
+
+    @Test
+    public void testHasStrongYinyong() {
+        Runnable hasStrongYinYong = new Runnable() {
+            @Override
+            public void run() {
+                Thread curThread = Thread.currentThread();
+
+                //1.先给当前线程设置一个threadLocal,
+                // 形式：使用变量接收 new ThreadLocal()，类似 XX x = new XX() 使用x变量保证存在强引用
+                //以验证 存在强引用情况下，gc 前后，
+                // ThreadLocalMap 的Entry[]数组里，以这个对象(new ThreadLocal())为key的key会不会被回收
+                ThreadLocal<String> threadLocal = new ThreadLocal<>();
+                threadLocal.set("业务数据");
+
+                try {
+                    log.info("gc前，查看 new ThreadLocal()形式的key在Entry数组中是否为null");
+                    showEntryInThread(curThread);
+
+                    log.info("--------------------------");
+
+                    log.info("gc后，查看 new ThreadLocal()形式的key在Entry数组中是否为null");
+                    System.gc();
+                    showEntryInThread(curThread);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        new Thread(hasStrongYinYong).start();
+
+        ClockUtil.sleep(1000);
+    }
 }
--- javaBasic/src/main/java/concurrent/core2/threadlocal/ThreadLocalDemo.java
+++ javaBasic/src/main/java/concurrent/core2/threadlocal/ThreadLocalDemo.java
@@ -11,7 +11,10 @@ import java.util.concurrent.Executors;
 
 @Data
 @Slf4j
+//基础demo,线程池每个线程创建一份数据 存放到 ThreadLocal 中
 public class ThreadLocalDemo {
+    //整个main的退出要等到所有线程执行完的1分钟之后
+    //等到cachedThreadPool的所有线程全部退出(cachedThreadPool默认是线程缓存1分钟/生命周期1分钟)
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
 
@@ -21,9 +24,12 @@ public class ThreadLocalDemo {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
+
+                    Thread thread = Thread.currentThread();
+
                     UserInfo userInfo = threadLocal.get();
                     if (Objects.isNull(userInfo)) {
-                        threadLocal.set(new UserInfo("lisi", 1));
+                        threadLocal.set(new UserInfo(thread.getName(), 1));
                     }
 
                     log.info("user={}", threadLocal.get());
